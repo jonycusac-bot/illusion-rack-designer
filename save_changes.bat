a29abb12c1e12b183fdfe43009b55abed15c6bc6
Illusion <illusion@e-illusion.es>
2026-08-13 17:24:32 +0200

    Agosto, mejoras1 - base path Vercel corregido

---
+++ save_changes.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "C:\Users\Usuario\Desktop\illusion-rack-designer-github"
+"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" commit -m "Agosto, mejoras1 - base path Vercel corregido"
+"C:\Program Files\Git\cmd\git.exe" push origin Racks-1
