02d7d5b0815e69644ee8d61499187ff3d1d0023a
Illusion <illusion@e-illusion.es>
2026-08-13 18:52:44 +0200

    Agosto 13 - nuevos equipos, consumos actualizados, ventiladores visibles, sonidos a├▒adir/eliminar

--- save_agosto13.bat
+++ save_agosto13.bat
@@ -1,5 +1,5 @@
 @echo off
 cd "C:\Users\Usuario\Desktop\illusion-rack-designer-github"
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Agosto, mejoras1 - base path Vercel corregido"
+"C:\Program Files\Git\cmd\git.exe" commit -m "Agosto 13 - nuevos equipos, consumos actualizados, ventiladores visibles, sonidos añadir/eliminar"
 "C:\Program Files\Git\cmd\git.exe" push origin Racks-1
